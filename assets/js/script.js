25895f3c62d4ae5769d9690771eb2c61311b8ecb
Anna Gerasimenko <annagerasimenko@MacBook-Pro-Anuar.local>
2023-02-15 21:25:02 +0600

    feat: add functions for showing time and date, greetings, image slider

---
+++ assets/js/script.js
@@ -0,0 +1,150 @@
+const time = document.querySelector('.time');
+const data = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+let posInitial;
+const bgNum  = getRandom().toString().padStart(2, "0");
+const body = document.getElementsByTagName('body')[0];
+let randomNum = parseInt(bgNum);
+const timeOfDay = getTimeOfDay();
+
+
+
+// show time in page
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+
+    time.textContent = currentTime;
+    showDate();
+    showGreeting();
+    setInterval(showTime, 0);
+  }
+  showTime();
+
+
+
+// show date in page
+function showDate() {
+  const date = new Date();
+  const options = {weekday: 'long', month: 'long', day: 'numeric'};
+  const currentDate = date.toLocaleDateString('en-En', options);
+
+  data.textContent = currentDate;
+}
+showDate()
+
+//greeting
+function getTimeOfDay() {
+  
+  const date = new Date();
+  let partOfDay = ['morning', 'afteroon', 'evening', 'night']
+  let hours = date.getHours();
+  
+    if (hours < 13 ) { 
+        return hours = partOfDay[0];
+    } else if (hours < 18 ) {
+        return  hours = partOfDay[1];
+    } else if (hours < 22 ) {
+       return hours = partOfDay[2];
+    } else {
+       return hours = partOfDay[3];
+    }
+  }
+getTimeOfDay();
+
+function showGreeting() {
+  const timeOfDay = getTimeOfDay();
+  const greetingText = `Good ${timeOfDay}`;
+  greeting.textContent = greetingText;
+}
+showGreeting();
+
+
+
+// save name user using localStorage
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage)
+
+
+// image change slider
+
+function getRandom() {
+  return Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+}
+// console.log(getRandom());
+
+function setBg() {
+  const img = new Image();
+  const timeOfDay = getTimeOfDay();
+  const bgNum  = getRandom().toString().padStart(2, "0");
+  const body = document.getElementsByTagName('body')[0];
+  img.src = "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+bgNum+".jpg";
+  img.onload = () => {      
+    body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+bgNum+".jpg')";
+  }; 
+}
+console.log(bgNum);
+setBg();
+
+
+// image flipping slider next & prev
+
+function getSlideNext() {
+  slideNext.addEventListener('click', getSlideNext => {
+      if(randomNum + 1  < 10) {
+        randomNum += 1;
+        body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+0+""+randomNum+".jpg')";
+        console.log(randomNum)
+      
+      } else if(randomNum + 1  > 9 && randomNum + 1  <= 20) {
+        randomNum += 1;
+        body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+randomNum+".jpg')";
+        console.log(randomNum)
+      
+      } else if (randomNum = 20) {
+        randomNum = 1;
+        body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+0+""+randomNum+".jpg')";
+        console.log(randomNum)
+      
+      } else
+      body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/01.jpg')";
+  })
+}
+getSlideNext();
+
+
+function getSlidePrev() {
+  slidePrev.addEventListener('click', getSlidePrev => {
+      if(randomNum - 1  < 10 && randomNum - 1  > 0) {
+        randomNum -= 1;
+        body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+0+""+randomNum+".jpg')";
+        console.log(randomNum)
+      
+      } else if(randomNum - 1  > 0 && randomNum - 1  <= 20 ) {
+        randomNum -= 1;
+        body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+randomNum+".jpg')";
+        console.log(randomNum)
+      
+      } else if (randomNum = 1) {
+        randomNum = 20;
+        body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/"+randomNum+".jpg')";
+        console.log(randomNum)
+      
+      } else
+      body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"+timeOfDay+"/01.jpg')";
+  })
+}
+getSlidePrev();
+
